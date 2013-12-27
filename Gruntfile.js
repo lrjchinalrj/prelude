f419d3ecb9534967de875a6671038c729c842926
Kaptinlin <kaptinlin@gmail.com>
2013-12-27 18:13:42 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,155 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/* <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    // -- copy config ----------------------------------------------------------
+    copy: {
+        adaptGrid: {
+            files: [{
+                    expand: true,
+                    cwd: 'bower_components/adaptGrid/less/',
+                    src:  '**',
+                    dest: 'less/functions/grid/'
+                }
+            ]
+        },
+        normalize: {
+          files: [{
+            src: 'bower_components/normalize-css/normalize.css', 
+            dest: 'less/common/reset/reset.less'
+          }]
+        },
+        rainbow: {
+          files: [{
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/rainbow/js',
+            src: [
+                'rainbow.min.js',
+            ],
+            dest: 'test/js/highlighting/'
+          },
+          {
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/rainbow/js/language',
+            src: [
+                'html.js'
+            ],
+            dest: 'test/js/highlighting/'
+          },
+          {
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/rainbow/themes',
+            src: [
+                'github.css'
+            ],
+            dest: 'test/css/highlighting/'
+          }]
+        },
+        lesshat: {
+          files: [{
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/lesshat/build',
+            src: [
+                'lesshat.less'
+            ],
+            dest: 'less/mixins/'
+          }]
+        }
+    },
+    
+    // -- Clean Config ---------------------------------------------------------
+
+    clean: {
+        css    : ['css/'],
+        release  : ['release/']
+    },
+
+    // -- Less Config ----------------------------------------------------------
+
+    less: {
+      dist: {
+        files: {
+          "css/prelude.css": "less/prelude.less"
+        }
+      }
+    },
+
+    // -- CSSLint Config -------------------------------------------------------
+
+    csslint: {
+        options: {
+            csslintrc: '.csslintrc'
+        },
+
+        src: {
+            src: [
+                'css/**/*.css'
+            ]
+        }
+    },
+
+    // -- CSSMin Config --------------------------------------------------------
+
+    cssmin: {
+        options: {
+            // report: 'gzip'
+        },
+
+        files: {
+            expand: true,
+            src   : 'css/*.css',
+            ext   : '-min.css'
+        }
+    },
+
+    // -- CSSMin Config --------------------------------------------------------
+    csscomb: {
+      sort: {
+        options: {
+          sortOrder: '.csscomb.json'
+        },
+        files: {
+          'css/prelude.css': ['css/prelude.css'],
+        }
+      }
+    },
+
+    // -- Watch/Observe Config -------------------------------------------------
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    },
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-csscomb');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'css']);
+  grunt.registerTask('css', ['less','csscomb','cssmin']);
+};
---
+++ README.md
@@ -0,0 +1,50 @@
+# Prelude
+
+Prelude is a flexable front-end development framework.
+
+Getting Started
+---------------
+Download the [production version][min] or the [development version][max].
+[min]: https://raw.github.com/amazingSurge/prelude/master/css/prelude-min.css
+[max]: https://raw.github.com/amazingSurge/prelude/master/css/prelude.css
+
+In your web page:
+
+```html
+<link rel="stylesheet" href="css/prelude-min.css">
+```
+
+Build From Source
+-----------------
+Optionally, you can build Prelude from its source on Github. To do this, you'll
+need to have Node.js and npm installed. We use [Grunt][] to build Prelude.
+[Grunt]: http://gruntjs.com/
+
+```shell
+$ git clone git://github.com/amazingSurge/prelude.git
+$ cd prelude
+$ npm install
+$ grunt
+```
+
+Documentation
+-------------
+_(Coming soon)_
+
+Release History
+---------------
+_(Nothing yet)_
+
+## Credits
+
+We built Prelude using popular open source projects.
+
+* jQuery ([MIT License](http://opensource.org/licenses/MIT))
+* normalize.css ([MIT License](http://opensource.org/licenses/MIT))
+* uikit ([MIT License](http://opensource.org/licenses/MIT))
+* foundation ([MIT License](http://opensource.org/licenses/MIT))
+
+License
+-------
+Copyright (c) 2013 amazingSurge  
+Licensed under the GPL license.
